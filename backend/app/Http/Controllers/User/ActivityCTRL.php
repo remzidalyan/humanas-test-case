5c184ae4447d3a55afa28b09885b6ba47e4d3c36
remzi.dalyan <rdalyan16@posta.pau.edu.tr>
2025-05-13 21:52:20 +0300

    Laravel controller dosyası düzenlendi.

--- backend/app/Http/Controllers/User/ActivityCTRL.php
+++ backend/app/Http/Controllers/User/ActivityCTRL.php
@@ -20,32 +20,59 @@ class ActivityCTRL extends Controller
 
             $data = json_decode($response->body());
             collect($data->data->rows)->each(function ($user) {
-                $logins = collect($user->logins)->map(fn($date) => Carbon::parse($date))->sort();
-                $user->message = '';
-                if ($logins->count() < 2) {
-                    $user->message = 'Not enough logins';
-                }
-
-                $dayDiffs = $logins
-                    ->skip(1)
-                    ->values()
-                    ->map(function ($login, $index) use ($logins) {
-                        return $login->diffInDays($logins[$index]);
-                    });
-
-                $averageDiff = round($dayDiffs->avg(), 2);
-                $lastLogin = $logins->last();
-                $predictedNextLogin = $lastLogin->copy()->addDays(round($averageDiff));
-
-                $user->average_range = [
-                    'last_login' => $lastLogin,
-                    'average_interval_days' => $averageDiff,
-                    'predicted_next_login' => $predictedNextLogin,
-                ];
+                $this->averageRange($user);
+                $this->linearRegression($user);
             });
             return response()->json($data);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Failed to fetch data', 'exception' => $e], 500);
         }
+    }
+
+    /**
+     * Bu fonksiyon kullanıcının giriş tarihlerini alır ve ortalama aralığı hesaplar.
+     * Tahmini bir sonraki giriş tarihini belirler.
+     *
+     * @param $user
+     * @return void
+     */
+    private function averageRange($user): void
+    {
+        $logins = collect($user->logins)->map(fn($date) => Carbon::parse($date))->sort();
+        $dayDiffs = $logins
+            ->skip(1)
+            ->values()
+            ->map(function ($login, $index) use ($logins) {
+                return $login->diffInDays($logins[$index]);
+            });
+
+        $averageDiff = round($dayDiffs->avg(), 2);
+        $lastLogin = $logins->last();
+        $predictedNextLogin = $lastLogin->copy()->addDays(round($averageDiff));
+
+        $user->average_range = [
+            'last_login' => $lastLogin,
+            'average_interval_days' => $averageDiff,
+            'predicted_next_login' => $predictedNextLogin,
+        ];
+    }
+
+    /**
+     * Bu fonksiyon kullanıcının giriş tarihlerini alır.
+     * Doğrusal regresyon tahmini yapan servise giriş bilgilerini yollar.
+     * Elde ettiği bilgiyi servis eder.
+     *
+     * @param $user
+     * @return void
+     */
+
+    private function linearRegression($user): void
+    {
+        $logins = collect($user->logins)->map(fn($date) => Carbon::parse($date))->sort();
+        $LRResponse = Http::post(env('ML_API_URL') . '/api/linear-regression/predict', [
+            'logins' => $logins->map(fn($date) => $date->format('Y-m-d\TH:i:s.u\Z'))->toArray(),
+        ]);
+
+        $user->linear_regression = json_decode($LRResponse->body());
     }
 }
